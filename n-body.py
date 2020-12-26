cf174745e62280c261e6c57491a61b90fa652287
JavoJavo <javojavojavojavo@gmail.com>
2020-12-26 13:31:25 -0600

    Add files via upload

---
+++ n-body.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python3
+#
+# n-body.py Solve the n-body problem using Newton
+# 
+# Copyright (C) 2019  Victor De la Luz (vdelaluz@enesmorelia.unam.mx)
+#                      
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import math
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+
+
+G=6.674e-11         #m^3kg^-1s^-2
+
+class Particle:
+    
+    def __init__(self, p, v, m, dt=1):
+        self.p = p #position
+        self.v = v #velocity
+        self.m = m #mass
+        self.dt = dt
+        self.trajectory = [p]
+        self.time = [0.0]
+
+    def setdt(self,dt):
+        self.dt = dt
+
+    def computeR(self,p1):
+        r = math.sqrt( (p1[0]-self.p[0])**2 + (p1[1]-self.p[1])**2 + (p1[2]-self.p[2])**2)
+        return r
+
+    def computeU(self,p1):
+        u=[0,0,0]
+        i=0
+        for a,b in zip(self.p,p1):
+            u[i] = b - a
+            i+=1
+        return u
+    
+    #def integrate(self,dt,p1,m1):
+    def integrate(self,B):
+        r = self.computeR(B.p)
+        u = self.computeU(B.p)
+
+        Vx=(G*B.m*self.dt/(r**3))*u[0]
+        Vy=(G*B.m*self.dt/(r**3))*u[1]
+        Vz=(G*B.m*self.dt/(r**3))*u[2]
+
+        
+        self.v[0] += Vx
+        self.v[1] += Vy
+        self.v[2] += Vz
+        
+        self.p = [self.p[0]+ (self.v[0]) *dt,self.p[1]+ (self.v[1])*dt,self.p[2]+ (self.v[2])*dt]
+
+    def getPosition(self):
+        return self.p
+
+    def getVelocity(self):
+        return self.v
+
+    def getKineticEnergy(self):
+        k= (1/2)*self.m*(math.sqrt( self.v[0]^2 +self.v[1]^2+self.v[2]^2))
+        return k    
+
+    #def integrate(self,dt,p1,m1):
+    def computeV(self,B):
+        r = self.computeR(B.p)
+        u = self.computeU(B.p)
+
+        Vx=(G*B.m*self.dt/(r**3))*u[0]
+        Vy=(G*B.m*self.dt/(r**3))*u[1]
+        Vz=(G*B.m*self.dt/(r**3))*u[2]
+        #print(u)
+        #print(r)
+        #print((G*B.m/(r**3))*u[0],(G*B.m/(r**3))*u[1],(G*B.m/(r**3))*u[2])         
+        return [Vx,Vy,Vz]
+
+
+    #def integrate(self,dt,p1,m1):
+    def updateV(self,v):
+        self.v[0] += v[0]
+        self.v[1] += v[1]
+        self.v[2] += v[2]
+        
+    #def integrate(self,dt,p1,m1):
+    def updatePosition(self,time,save):        
+        self.p = [self.p[0]+ (self.v[0]) *dt,self.p[1]+ (self.v[1])*dt,self.p[2]+ (self.v[2])*dt]
+        if save:
+            self.time.append(time)
+            self.trajectory.append(self.p)
+
+
+    def getTrajectory(self):
+        return self.time, self.trajectory
+        
+class Potential:
+    
+    def __init__(self, system, dt):
+        self.system = system #set of Particles
+        self.dt = dt #set of Particles
+
+    def integrate(self,time,save):
+        #print(time/3600.0/24.0)
+        for particle in self.system:
+            for other in self.system:
+                if other != particle:
+                    velocity = particle.computeV(other)
+                    particle.updateV(velocity)
+        for particle in self.system:
+            particle.updatePosition(time,save)
+
+        return self.system
+
+from decimal import *
+import math
+lenTime=3600.0*24*30*2  #sec (2 meses)
+lenTime=60*60*2 #  en segundos (periodo orbital de Mimas: 23 horas)
+dt=0.50      #sec    
+
+
+# Saturno es el marco de referencia del sistema, por eso está en el origen y no tiene velocidades
+saturn = Particle([0,0,0],[0,0,0],5.6834E+26)
+'''
+X =-7.336432675167782E-04*1.496e+11 
+Y = 9.029757744771170E-04*1.496e+11 
+Z =-4.400017633666113E-04*1.496e+11
+VX=-6.722716075791823E-03*1.496e+11 
+VY=-3.906534056535731E-03*1.496e+11 
+VZ= 2.741552242697911E-03*1.496e+11
+'''
+X =-1.097514706739005E+05
+Y = 1.350832531554601E+05
+Z =-6.582332690389031E+04
+VX=-1.164009271133237E+01
+VY=-6.763995100402531E+00
+VZ= 4.746879374078901E+00
+mimas = Particle([X,Y,Z],[VX,VY,VZ],3.75E+19)
+#mimas = Particle([X,Y,Z],[0,0,0],3.75E+19)
+n_steps = int(lenTime/dt)
+
+particles = [saturn,mimas]
+
+twoBody = Potential(particles,dt)
+
+x=[]
+y=[]
+
+
+skip=0
+save=False
+#n_steps = 3
+
+print(str(1)+'/'+str(n_steps))
+for t in range(1,n_steps):
+	print(str(t+1)+'/'+str(n_steps))
+	if skip == 1000:
+	    skip=0
+	    save=True
+	system = twoBody.integrate(float(t)*dt,save)
+	save=False
+	skip += 1
+	#if t==1000000:
+	#	break
+
+#B.setdt(dt)
+#x=[]
+#y=[]
+#v=[]
+#a=[]
+#x.append(0.0)
+##y.append(B.getPosition()[0])
+#y.append(B.getPosition())
+#v.append(B.getVelocity()[0])
+#
+#print(B.getVelocity()[0])
+#
+#a.append(0.0)
+#v1=B.getVelocity()[0]
+##lastX = B.getPosition()[0]
+#
+##for t in range(1,100):
+##    lastX = B.getPosition()[0]
+##    lastV = v1
+##    B.integrate(A)
+##    print(B.getPosition())
+##    x.append(float(t)*dt)
+##    y.append(B.getPosition()[0])
+##    v1=(B.getPosition()[0]-lastX)/B.dt
+##    print(v1)
+##    v.append(v1)
+##    a.append((v1-lastV)/B.dt)
+#
+#for t in range(1,100):
+#    B.integrate(A)
+#    x.append(float(t)*dt)
+#    y.append(B.getPosition())
+
+fig = plt.figure()
+
+ax = fig.add_subplot(111, projection='3d')
+
+i=0
+c=['g','r','b','g','r','b','g','r','b','g','r','b']
+for particle in particles:
+    time, trajectory = particle.getTrajectory()
+    for x, y in zip(time,trajectory):
+        ax.scatter(y[0], y[1], y[2], marker='o',c=c[i])
+        #ax.scatter(y[0], y[1], y[2], c=c[i])
+    i=i+1
+
+
+# stack the plots
+#lns = []
+#for i in range(len(t)):
+#    ln1, = ax.plot(y0[:i], y1[:i], z1[:i], 'o-', color='steelblue')
+#    ln2, = ax.plot(x2[:i], y2[:i], z2[:i], 'o-', color='darkorange')
+#    lns.append([ln1, ln2])
+
+#line_ani = animation.ArtistAnimation(fig, lns, interval=100, blit=True)
+
+    
+#for point in y:
+#    ax.scatter(point[0], point[1], point[2], marker='o')
+
+#pointA=A.getPosition()
+#ax.scatter(pointA[0], pointA[1], pointA[2], marker='o')
+
+    
+#fig, ax = plt.subplots(3)    
+#ax[0].plot(x,y)
+#ax[0].set(xlabel='time [sec]', ylabel='distance [km]',
+#           title="n-body")
+#ax[0].grid()
+#
+#ax[1].plot(x,v)
+#ax[1].set(xlabel='time [sec]', ylabel='velocity [km/s]')
+#ax[1].grid()
+#
+#ax[2].plot(x,a)
+#ax[2].set(xlabel='time [sec]', ylabel='acceleration [km/s^2]')
+#ax[2].grid()
+
+
+
+plt.show()
+
+
+
